fa7486cf761d342bd5ff248c13721b1fcc74e606
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-11-13 23:12:01 +0100

    Create cam-config.php

---
+++ zoneminder/cam-config.php
@@ -0,0 +1,13 @@
+<?php
+// -------------------------------------------------------
+//        Credentials for ZoneMinder camera wall
+// -------------------------------------------------------
+
+// zoneminder configuration
+$zmUser = "youruser";
+$zmPass = "yourpassword";
+$zmSecret = "yoursecret";
+
+// zoneminder URL on LAN
+$zmURL  = "http://127.0.0.1/zm";
+?>
