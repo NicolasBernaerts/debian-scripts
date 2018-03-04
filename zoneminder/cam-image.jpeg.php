dd75e6f3f3f765b551c85585da7884f6d83d3ab9
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-03-04 15:49:19 +0100

    Update cam-image.jpeg.php

--- zoneminder/cam-image.jpeg.php
+++ zoneminder/cam-image.jpeg.php
@@ -9,18 +9,21 @@
 // zoneminder configuration
 require_once ("cam-config.inc");
 
-// parameters : image index and image scale (in % between 1 and 100)
+// parameters : image index
 $monitorId = -1;
 if (isset($_GET["id"])) $monitorId = $_GET["id"];
+
+// parameter : image scale (in % between 1 and 100)
 $monitorScale = 0;
 if (isset($_GET["scale"])) $monitorScale = $_GET["scale"];
 
-// calculate hash login
+// calculate hash login ($zmSecret . $zmUser . $zmPass . $arrTime['tm_hour'] . $arrTime['tm_mday'] . $arrTime['tm_mon'] . $arrTime['tm_year'])
 $arrTime = localtime();
 $authKey = $zmSecret . $zmUser . $zmPass . $arrTime[2] . $arrTime[3] . $arrTime[4] . $arrTime[5];
 $authHash = md5 ($authKey);
 
 // generate zoneminder URL
+//$imgURL = $zmURL . "/cgi-bin/zms?mode=single&monitor=" . $monitorId . "&scale=" . $monitorScale . "&auth=" . $authHash;
 $imgURL = $zmURL . "/cgi-bin/zms?mode=single&monitor=" . $monitorId . "&scale=" . $monitorScale . "&user=" . $zmUser . "&pass=" . $zmPass;
 
 // set jpeg header
