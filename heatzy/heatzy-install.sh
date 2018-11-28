b445752e0381b187b06a1f0714cb81ffd60ef7dc
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-11-29 00:14:32 +0100

    Update heatzy-install.sh

--- heatzy/heatzy-install.sh
+++ heatzy/heatzy-install.sh
@@ -3,9 +3,16 @@
 # if absent, create config directory
 mkdir --parents $HOME/.config
 
+# install icon
+sudo wget -O /usr/share/icons/heatzy.png https://raw.githubusercontent.com/NicolasBernaerts/icon/master/heatzy.png
+
 # retrieve configuration file
 wget -O $HOME/.config/heatzy.conf https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy.conf
 
-# install main script
+# install heatzy script
 sudo wget -O /usr/local/sbin/heatzy https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy
 sudo chmod +x /usr/local/sbin/heatzy
+
+# install heatzy-gui script
+sudo wget -O /usr/local/sbin/heatzy-gui https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy-gui
+sudo chmod +x /usr/local/sbin/heatzy-gui
