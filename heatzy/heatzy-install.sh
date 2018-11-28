f752f9d5dc6e72dde2f886a11f2a9f539105b6b7
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2018-11-29 00:19:11 +0100

    Update heatzy-install.sh

--- heatzy/heatzy-install.sh
+++ heatzy/heatzy-install.sh
@@ -16,3 +16,7 @@ sudo chmod +x /usr/local/sbin/heatzy
 # install heatzy-gui script
 sudo wget -O /usr/local/sbin/heatzy-gui https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy-gui
 sudo chmod +x /usr/local/sbin/heatzy-gui
+
+# desktop integration
+sudo wget -O /usr/share/applications/heatzy.desktop https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy.desktop
+sudo chmod +x /usr/share/applications/heatzy.desktop
