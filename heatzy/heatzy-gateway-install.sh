000233e613c9dcaad88910de39d42c95e5249f57
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-02-21 19:39:46 +0100

    Update heatzy-gateway-install.sh

--- heatzy/heatzy-gateway-install.sh
+++ heatzy/heatzy-gateway-install.sh
@@ -7,7 +7,5 @@ chmod +x /usr/local/sbin/heatzy-mqtt-gateway
 # declare service
 wget -O /etc/systemd/system https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/heatzy/heatzy-mqtt-gateway.service
 
-# enable and start service
+# enable the gateway service
 systemctl enable heatzy-mqtt-gateway.service
-systemctl start heatzy-mqtt-gateway.service
-systemctl status heatzy-mqtt-gateway.service
