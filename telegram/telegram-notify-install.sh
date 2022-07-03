c82b4acc41b59d2c5fbd3847707372cddfcd47f6
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2022-07-03 12:47:20 +0200

    Update telegram-notify-install.sh

--- telegram/telegram-notify-install.sh
+++ telegram/telegram-notify-install.sh
@@ -5,5 +5,9 @@ apt-get install curl
 
 # retrieve configuration file and main script
 wget -O /etc/telegram-notify.conf https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify.conf
-wget -O /usr/local/sbin/telegram-notify https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify
-chmod +x /usr/local/sbin/telegram-notify
+wget -O /usr/local/bin/telegram-notify https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/telegram-notify
+chmod +x /usr/local/bin/telegram-notify
+
+# retrieve rsync wrapper
+wget -O /usr/local/bin/rsync https://raw.githubusercontent.com/NicolasBernaerts/debian-scripts/master/telegram/rsync
+chmod +x /usr/local/bin/rsync
